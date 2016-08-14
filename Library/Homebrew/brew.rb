0263a60463056c437d0a695187f9eda1a7cb8e59
AnastasiaSulyagina <anastasia.sulyagina@gmail.com>
2016-08-14 16:23:06 +0300

    init

--- Library/Homebrew/brew.rb
+++ Library/Homebrew/brew.rb
@@ -54,6 +54,9 @@ begin
     ENV["PATH"] += "#{File::PATH_SEPARATOR}#{tap_cmd_dir}"
   end
 
+  # Add cask cmd.
+  ENV["PATH"] += "#{File::PATH_SEPARATOR}#{HOMEBREW_LIBRARY}/Homebrew/cask/cmd"
+
   # Add SCM wrappers.
   ENV["PATH"] += "#{File::PATH_SEPARATOR}#{HOMEBREW_SHIMS_PATH}/scm"
 
---
+++ Library/Homebrew/cask/test/support/Casks/caffeine.rb
@@ -0,0 +1,11 @@
+cask 'caffeine' do
+  version ''
+  sha256 ''
+
+  url 'https://'
+  name ''
+  homepage ''
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app ''
+end
